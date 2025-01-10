b43aa3775e4b5777140107aaa44509562c0ac05c
AxVsel <assperacus77@gmail.com>
2025-01-10 23:09:15 +0800

    fix deployment 1

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,8 @@
 const { Sequelize, QueryTypes, where } = require("sequelize");
-const config = require("../config/config.json");
-const sequelize = new Sequelize(config.development);
+require("dotenv").config();
+const environment = process.env.NODE_ENV;
+const config = require("../config/config");
+const sequelize = new Sequelize(config[environment]);
 const { Blog, User, Myproject } = require("../models");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
